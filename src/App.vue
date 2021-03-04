30b949d2ba9168fc758be0cfdb6b2ea45b67619a
Wouter de Bruijn <git@electrogamez.nl>
2021-03-04 15:50:35 +0100

    :sparkles: Added Roboto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700;900&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -26,5 +23,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+html {
+  background-color: #333;
 }
 </style>
