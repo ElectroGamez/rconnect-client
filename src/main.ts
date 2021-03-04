8255c264b6eeaffd64cfaf1e8a30b40ce1236e30
Wouter de Bruijn <git@electrogamez.nl>
2021-03-04 15:48:48 +0100

    :sparkles: Added axios to Vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { store } from "./store";
 
-createApp(App)
-  .use(store)
+import axios, { AxiosInstance } from "axios";
+
+const app = createApp(App);
+
+app
   .use(router)
+  .use(store)
   .mount("#app");
+
+app.config.globalProperties.$axios = axios.create({
+  baseURL: process.env.VUE_BACKEND_BASEURL ?? "http://localhost:5000/"
+});
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+  }
+}
